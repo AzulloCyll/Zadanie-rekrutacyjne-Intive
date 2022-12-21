de57839d90d67e962c21d85d9444ce6febadc25b
Daniel Chmur <daniel.chmur@gmail.com>
2022-12-21 16:13:45 +0100

    simple data sorting tested

--- src/index.js
+++ src/index.js
@@ -1,20 +1,24 @@
-const main = document.querySelector("main");
+const main = document.querySelector("main")
 
 const generateDataArticles = async (element) => {
-  let transactionData = await fetchTransactions();
+    let transactionData = await fetchTransactions()
 
-  // transactionData.sort((a,b)=> {transactionData.a })
+    console.log(transactionData)
 
-  element.innerHTML = "";
+    transactionData.sort((a, b) => {
+        return b.description.localeCompare(a.description)
+    })
 
-  for (data of transactionData) {
-    const div = document.createElement("div");
-    const p = document.createElement("p");
-    p.innerHTML = `Title = ${data.description}`;
-    div.classList.add("transaction");
-    div.append(p);
-    element.prepend(div);
-  }
-};
+    element.innerHTML = ""
 
-generateDataArticles(main);
+    for (data of transactionData) {
+        const div = document.createElement("div")
+        const p = document.createElement("p")
+        p.innerHTML = `Title = ${data.description}, Amount = ${data.amount}`
+        div.classList.add("transaction")
+        div.append(p)
+        element.prepend(div)
+    }
+}
+
+generateDataArticles(main)
