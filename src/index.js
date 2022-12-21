85f80ad7465604dcc28740b8c891cd3a30d5b8cb
Daniel Chmur <daniel.chmur@gmail.com>
2022-12-21 14:55:00 +0100

    data

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
-class Bank {
-  showTransactions() {
-    console.log(this.transactions);
+const main = document.querySelector("main");
+
+const generateDataArticles = async (element) => {
+  let transactionData = await fetchTransactions();
+
+  // transactionData.sort((a,b)=> {transactionData.a })
+
+  element.innerHTML = "";
+
+  for (data of transactionData) {
+    const div = document.createElement("div");
+    const p = document.createElement("p");
+    p.innerHTML = `Title = ${data.description}`;
+    div.classList.add("transaction");
+    div.append(p);
+    element.prepend(div);
   }
+};
 
-  async init() {
-    this.transactions = "500";
-  }
-}
-
-const myBank = new Bank();
-
-myBank.init();
-myBank.showTransactions();
+generateDataArticles(main);
