fa3fed3402535274d2f8d79c40e277f31581d99a
Daniel Chmur <daniel.chmur@gmail.com>
2022-12-22 13:22:30 +0100

    playing with grid

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const main = document.querySelector("main")
+const main = document.querySelector("main .top")
 
 const generateDataArticles = async (element) => {
     let transactionData = await fetchTransactions()
