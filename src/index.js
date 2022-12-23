2a9193134e7d4872547817cc95c9bea618002619
Daniel Chmur <daniel.chmur@gmail.com>
2022-12-23 10:21:40 +0100

    navigation working

--- src/index.js
+++ src/index.js
@@ -1,5 +1,44 @@
 const main = document.querySelector("main .top")
 
+const nav = document.querySelectorAll("body > header > nav > ul > li")
+const navLogoff = document.querySelector(
+    "body > header > nav > ul > li:last-child > span.button"
+)
+
+const loginButton = document.querySelector("body > main > div.login > button")
+
+const mainPages = document.querySelectorAll("main > div")
+
+const unsetHidden = (element) => {
+    element.classList.remove("hidden")
+}
+
+const setHidden = (element) => {
+    element.classList.add("hidden")
+}
+
+const hideAll = (elements) => {
+    for (elem of elements) {
+        elem.classList.add("hidden")
+    }
+}
+
+const showAll = (elements) => {
+    for (elem of elements) {
+        elem.classList.remove("hidden")
+    }
+}
+
+const setActive = (element) => {
+    element.classList.add("active")
+}
+
+const unsetActiveAll = (elements) => {
+    for (el of elements) {
+        el.classList.remove("active")
+    }
+}
+
 const generateDataArticles = async (element) => {
     let transactionData = await fetchTransactions()
 
@@ -21,4 +60,35 @@ const generateDataArticles = async (element) => {
     }
 }
 
-generateDataArticles(main)
+// generateDataArticles(main)
+
+nav[0].addEventListener("click", () => {
+    unsetActiveAll(nav)
+    hideAll(mainPages)
+
+    setActive(nav[0])
+    unsetHidden(mainPages[1])
+})
+
+nav[1].addEventListener("click", () => {
+    unsetActiveAll(nav)
+    hideAll(mainPages)
+
+    setActive(nav[1])
+    unsetHidden(mainPages[2])
+})
+
+navLogoff.addEventListener("click", () => {
+    unsetActiveAll(nav)
+    hideAll(mainPages)
+    showAll(nav)
+    setHidden(nav[2])
+    unsetHidden(mainPages[0])
+})
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault()
+    unsetActiveAll(nav)
+    hideAll(nav)
+    unsetHidden(nav[2])
+})
