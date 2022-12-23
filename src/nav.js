b4d122b157ded818c048ceac158926f06b6040ad
Daniel Chmur <daniel.chmur@gmail.com>
2022-12-23 10:40:15 +0100

    navigation logic moved to separate file

---
+++ src/nav.js
@@ -0,0 +1,69 @@
+const nav = document.querySelectorAll("body > header > nav > ul > li")
+const navLogoff = document.querySelector(
+    "body > header > nav > ul > li:last-child > span.button"
+)
+const loginButton = document.querySelector("body > main > div.login > button")
+const mainPages = document.querySelectorAll("main > div")
+
+const unsetHidden = (element) => {
+    element.classList.remove("hidden")
+}
+
+const setHidden = (element) => {
+    element.classList.add("hidden")
+}
+
+const hideAll = (elements) => {
+    for (elem of elements) {
+        elem.classList.add("hidden")
+    }
+}
+
+const showAll = (elements) => {
+    for (elem of elements) {
+        elem.classList.remove("hidden")
+    }
+}
+
+const setActive = (element) => {
+    element.classList.add("active")
+}
+
+const unsetActiveAll = (elements) => {
+    for (el of elements) {
+        el.classList.remove("active")
+    }
+}
+
+nav[0].addEventListener("click", () => {
+    unsetActiveAll(nav)
+    hideAll(mainPages)
+
+    setActive(nav[0])
+    unsetHidden(mainPages[1])
+})
+
+nav[1].addEventListener("click", () => {
+    unsetActiveAll(nav)
+    hideAll(mainPages)
+
+    setActive(nav[1])
+    unsetHidden(mainPages[2])
+})
+
+navLogoff.addEventListener("click", () => {
+    unsetActiveAll(nav)
+    hideAll(mainPages)
+    showAll(nav)
+    setHidden(nav[2])
+    unsetHidden(mainPages[0])
+})
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault()
+    unsetActiveAll(nav)
+    hideAll(nav)
+    hideAll(mainPages)
+    unsetHidden(nav[2])
+    unsetHidden(mainPages[3])
+})
