8e98a44b0afdb7bd3c4dc737a90c0c3f7b91834a
Daniel Chmur <daniel.chmur@gmail.com>
2023-01-05 23:55:18 +0100

    work on transactions in progress

--- src/dataViewGenerator.js
+++ src/dataViewGenerator.js
@@ -12,9 +12,14 @@ const generateDataArticles = async (element) => {
 
     element.innerHTML = ""
 
+    //dopisać paski z datą na mobile
     for (transaction of transactions) {
-        const div = document.createElement("div")
-        div.classList.add("transaction")
+        const article = document.createElement("article")
+        article.classList.add("transaction")
+
+        const date = document.createElement("span")
+        date.classList.add("date", "hidden")
+        date.innerHTML = transaction.date
 
         const icon = document.createElement("i")
         icon.classList.add("icon")
@@ -24,13 +29,21 @@ const generateDataArticles = async (element) => {
         desc.classList.add("description")
         desc.innerHTML = transaction.description
 
+        const transactionTypeName = document.createElement("span")
+        transactionTypeName.classList.add("type", "hidden")
+        transactionTypeName.innerHTML = transaction.type
+
         const amount = document.createElement("span")
         amount.classList.add("amount")
         amount.innerHTML = transaction.amount
 
-        div.append(icon, desc, amount)
+        const balance = document.createElement("span")
+        balance.classList.add("balance", "hidden")
+        balance.innerHTML = transaction.balance
 
-        element.append(div)
+        article.append(date, icon, desc, transactionTypeName, amount, balance)
+
+        element.append(article)
     }
 }
 
@@ -45,3 +58,29 @@ const addId = (transactions) => {
     }
     return result
 }
+
+const showElements = (...elements) => {
+    for (let element of elements) {
+        showAll(element)
+    }
+}
+
+const hideElements = (...elements) => {
+    for (let element of elements) {
+        hideAll(element)
+    }
+}
+
+const dates = document.getElementsByClassName("date")
+const transactionTypeNames = document.getElementsByClassName("type")
+const balances = document.getElementsByClassName("balance")
+
+addEventListener("resize", () => {
+    if (window.innerWidth > 769) {
+        console.log("mobile")
+        showElements(dates, transactionTypeNames, balances)
+    } else {
+        console.log("desktop")
+        hideElements(dates, transactionTypeNames, balances)
+    }
+})
